d2427a7c6b962a418b390961d29f7540aaee46cf
Carlos Godoy <ct_godoy@outlook.com>
2022-01-05 16:47:52 -0600

    feat(View): Add link to views

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -20,4 +20,54 @@ router.get('/principal_menu', (req, res) => {
   res.render('principal_menu');
 });
 
+// ENTRADAS VIEW
+router.get('/entradas', (req, res) => {
+  res.render('entradas');
+});
+
+// TACOS VIEW
+router.get('/tacos', (req, res) => {
+  res.render('tacos');
+});
+
+// EMPANADAS VIEW
+router.get('/empanadas', (req, res) => {
+  res.render('empanadas');
+});
+
+// HAMBURGUESAS VIEW
+router.get('/hamburguesas', (req, res) => {
+  res.render('hamburguesas');
+});
+
+// PEPITOS VIEW
+router.get('/pepitos', (req, res) => {
+  res.render('pepitos');
+});
+
+// CORTES VIEW
+router.get('/cortes', (req, res) => {
+  res.render('cortes');
+});
+
+// PARRILLADAS VIEW
+router.get('/parrilladas', (req, res) => {
+  res.render('parrilladas');
+});
+
+// MARISCOS VIEW
+router.get('/mariscos', (req, res) => {
+  res.render('mariscos');
+});
+
+// BEBIDAS VIEW
+router.get('/bebidas', (req, res) => {
+  res.render('bebidas');
+});
+
+// POSTRES VIEW
+router.get('/postres', (req, res) => {
+  res.render('postres');
+});
+
 module.exports = router;
