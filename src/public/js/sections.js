7fa426ae03de6b03c07edd11453838f5bc28682d
Carlos Godoy <ct_godoy@outlook.com>
2022-01-17 16:55:03 -0600

    feat(logic):ASdd function to sections menu

--- src/public/js/sections.js
+++ src/public/js/sections.js
@@ -1,14 +1,117 @@
-console.log('The js is correct!');
+function agregarProducto(btnQuitar, btnAgregar, inputCantidad) {
+  let btnQuitarProducto = document.getElementById(btnQuitar);
+  let btnAgregarProducto = document.getElementById(btnAgregar);
+  let inputCantidadProducto = document.getElementById(inputCantidad);
+  console.log(inputCantidadProducto);
+  let value = inputCantidadProducto.value;
 
-let numContainer = document.getElementById('contadorGuacamole');
-let value = 0;
-let btnInc = document.getElementById('agregar-guacamole');
-
-console.log(numContainer);
-
-btnInc.addEventListener("click", (event) => {
+  btnQuitarProducto.addEventListener('click', (event) => {
     event.preventDefault();
-    value++;
-    // numContainer.textContent = value;
-    numContainer.value = value;
-});
+
+    if (value != 0) {
+      value--;
+      inputCantidadProducto.value = value;
+    }
+  });
+
+  btnAgregarProducto.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (value < 25) {
+      value++;
+      inputCantidadProducto.value = value;
+    }
+  })
+}
+
+
+// AGREGAR ENTRADAS
+let entradasSection = document.getElementById('entradasSection');
+
+if (entradasSection) {
+  let entradaGuacamole = agregarProducto('quitarGuacamole', 'agregarGuacamole', 'contadorGuacamole');
+  let entradaEnsaladaPalmito = agregarProducto('quitarEnsaladaPalmito', 'agregarEnsaladaPalmito', 'contadorEnsaladaPalmito');
+  let entradaEnsaladaAguacate = agregarProducto('quitarEnsaladaAguacate', 'agregarEnsaladaAguacate', 'contadorEnsaladaAguacate');
+  let entradaEnsaladaMixLechugas = agregarProducto('quitarEnsaladaMix', 'agregarEnsaladaMix', 'contadorEnsaladaMix');
+  let entradaCalamarCapeado = agregarProducto('quitarCalamarCapeado', 'agregarCalamarCapeado', 'contadorCalamarCapeado');
+  let entradaQuesoFundido = agregarProducto('quitarQuesoFundido', 'agregarQuesoFundido', 'contadorQuesoFundido');
+  let entradaQuesoFundidoChorizo = agregarProducto('quitarQuesoFundidoChorizo', 'agregarQuesoFundidoChorizo', 'contadorQuesoFundidoChorizo');
+  let entradaQuesoFundidoChistorra = agregarProducto('quitarQuesoFundidoChistorra', 'agregarQuesoFundidoChistorra', 'contadorQuesoFundidoChistorra');
+  let entradaTuetanos = agregarProducto('quitarTuetanos', 'agregarTuetanos', 'contadorTuetanos');
+  let entradaChistorra = agregarProducto('quitarChistorra', 'agregarChistorra', 'contadorChistorra');
+  let entradaMollejas = agregarProducto('quitarMollejas', 'agregarMollejas', 'contadorMollejas');
+  let entradaChicharronNewYork = agregarProducto('quitarChicharronNewYork', 'agregarChicharronNewYork', 'contadorChicharronNewYork');
+  let entradaVegetalesGrill = agregarProducto('quitarVegetalesGrill', 'agregarVegetalesGrill', 'contadorVegetalesGrill');
+  let entradaPapasFrancesa = agregarProducto('quitarPapasFrancesa', 'agregarPapasFrancesa', 'contadorPapasFrancesa');
+  let entradaPapasGajo = agregarProducto('quitarPapasGajo', 'agregarPapasGajo', 'contadorPapasGajo');
+  let entradaArosCebolla = agregarProducto('quitarArosCebolla', 'agregarArosCebolla', 'contadorArosCebolla');
+}
+
+// AGREGAR TACOS
+let tacosSection = document.getElementById('tacosSection');
+
+if (tacosSection) {
+  let tacoVacioAngus = agregarProducto('quitarTacosVacioAngus', 'agregarTacosVacioAngus', 'contadorTacosVacioAngus');
+  let tacoPicanaAngus = agregarProducto('quitarTacosPicanaAngus', 'agregarTacosPicanaAngus', 'contadorTacosPicanaAngus');
+  let tacoArrachera = agregarProducto('quitarTacosArrachera', 'agregarTacosArrachera', 'contadorTacosArrachera');
+  let tacoNewYork = agregarProducto('quitarTacosNewYork', 'agregarTacosNewYork', 'contadorTacosNewYork');
+  let tacoCostillaRes = agregarProducto('quitarTacosCostillaRes', 'agregarTacosCostillaRes', 'contadorTacosCostillaRes');
+  let tacoAtun = agregarProducto('quitarTacosAtun', 'agregarTacosAtun', 'contadorTacosAtun');
+}
+
+// EMPANADAS
+let empanadasSection = document.getElementById('empanadasSection');
+
+if (empanadasSection) {
+  let empanadaHumita = agregarProducto('quitarEmpanadasHumita', 'agregarEmpanadasHumita', 'contadorEmpanadasHumita');
+  let empanadaCarnalita = agregarProducto('quitarEmpanadasCarnalita', 'agregarEmpanadasCarnalita', 'contadorEmpanadasCarnalita');
+  let empanadaValenciana = agregarProducto('quitarEmpanadasValenciana', 'agregarEmpanadasValenciana', 'contadorEmpanadasValenciana');
+  let empanadaMediterranea = agregarProducto('quitarEmpanadasMediterranea', 'agregarEmpanadasMediterranea', 'contadorEmpanadasMediterranea');
+  let empanadaCampechana = agregarProducto('quitarEmpanadasCampechana', 'agregarEmpanadasCampechana', 'contadorEmpanadasCampechana');
+}
+
+// HAMBURGUESAS
+let hamburguesasSection = document.getElementById('hamburguesasSection');
+
+if (hamburguesasSection) {
+  let hamburguesaClasica = agregarProducto('quitarHamburguesaClasica', 'agregarHamburguesaClasica', 'contadorHamburguesaClasica');
+  let hamburguesaBigBen = agregarProducto('quitarHamburguesaBigBen', 'agregarHamburguesaBigBen', 'contadorHamburguesaBigBen');
+  let hamburguesaFridaKahlo = agregarProducto('quitarHamburguesaFridaKahlo', 'agregarHamburguesaFridaKahlo', 'contadorHamburguesaFridaKahlo');
+  let hamburguesaLadyBurguer = agregarProducto('quitarHamburguesaLadyBurger', 'agregarHamburguesaLadyBurger', 'contadorHamburguesaLadyBurger');
+  let hamburguesaMillenial = agregarProducto('quitarHamburguesaMillenial', 'agregarHamburguesaMillenial', 'contadorHamburguesaMillenial');
+  let hamburguesaBruceBurger = agregarProducto('quitarHamburguesaBruceBurger', 'agregarHamburguesaBruceBurger', 'contadorHamburguesaBruceBurger');
+}
+
+// PEPITOS
+let pepitosSection = document.getElementById('pepitosSection');
+
+if (pepitosSection) {
+  let pepitosVacioAngus = agregarProducto('quitarPepitosVacioAngus', 'agregarPepitosVacioAngus', 'contadorPepitosVacioAngus');
+  let pepitosArrachera = agregarProducto('quitarPepitosArrachera', 'agregarPepitosArrachera', 'contadorPepitosArrachera');
+  let pepitosPicanaAngus = agregarProducto('quitarPepitosPicanaAngus', 'agregarPepitosPicanaAngus', 'contadorPepitosPicanaAngus');
+  let pepitosRibEye = agregarProducto('quitarPepitosRibEye', 'agregarPepitosRibEye', 'contadorPepitosRibEye');
+  let pepitosChoripan = agregarProducto('quitarPepitosChoripan', 'agregarPepitosChoripan', 'contadorPepitosChoripan');
+  let pepitosNewYork = agregarProducto('quitarPepitosNewYork', 'agregarPepitosNewYork', 'contadorPepitosNewYork');
+  let pepitosCamaron = agregarProducto('quitarPepitosCamaron', 'agregarPepitosCamaron', 'contadorPepitosCamaron');
+}
+
+// CORTES
+let cortesSection = document.getElementById('cortesSection');
+
+if (cortesSection) {
+  let corteRibEye = agregarProducto('quitarCorteRibEye', 'agregarCorteRibEye', 'contadorCorteRibEye');
+  let corteVacioAngus = agregarProducto('quitarCorteVacioAngus', 'agregarCorteVacioAngus', 'contadorCorteVacioAngus');
+  let corteVacioArrachera = agregarProducto('quitarCorteArrachera', 'agregarCorteArrachera', 'contadorCorteArrachera');
+  let corteVacioPicanaAngus = agregarProducto('quitarCortePicanaAngus', 'agregarCortePicanaAngus', 'contadorCortePicanaAngus');
+  let corteVacioNewYork = agregarProducto('quitarCorteNewYork', 'agregarCorteNewYork', 'contadorCorteNewYork');
+}
+
+// PARRILLADAS
+let parrilladasSection = document.getElementById('parrilladasSection');
+
+if (parrilladasSection) {
+  let parrilladaCasaParrilla = agregarProducto('quitarParrillaCasaParrilla', 'agregarParrillaCasaParrilla', 'contadorParrillaCasaParrilla');
+  let parrilldasCanival = agregarProducto('quitarParrillaCanival', 'agregarParrillaCanival', 'contadorParrillaCanival');
+  let parrilladaCarniceria = agregarProducto('quitarParrillaCarniceria', 'agregarParrillaCarniceria', 'contadorParrillaCarniceria');
+  let parrilladaMarTierra = agregarProducto('quitarParrillaMarTierra', 'agregarParrillaMarTierra', 'contadorParrillaMarTierra');
+}
